0a5df853acca5b5c9d78a01bb02641471d3267ae
maritogonzalez442 <maritogonzales442@gmail.com>
2025-07-17 20:40:21 -0300

    Add .next/static/chunks/src_lib_storage_ts_25127b80._.js 1752795626029

---
+++ .next/static/chunks/src_lib_storage_ts_25127b80._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/lib/storage.ts [app-client] (ecmascript, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.resolve().then(() => {
+        return parentImport("[project]/src/lib/storage.ts [app-client] (ecmascript)");
+    });
+});
+}}),
+}]);
