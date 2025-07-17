fc3197af951952e8a51b45c590cb439ac57fee2d
maritogonzalez442 <maritogonzales442@gmail.com>
2025-07-17 20:41:01 -0300

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1752795665618

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_lib_storage_ts_53e6d65f._.js",
+  "static/chunks/_56a57708._.js"
+],
+    source: "dynamic"
+});
